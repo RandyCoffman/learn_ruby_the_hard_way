799de1e67b4923646f6029bd9a42f163cafd3422
RandyCoffman <randy.coffman@wvjc.edu>
2018-02-21 13:20:45 -0500

    first commit

--- ex40.rb
+++ ex40.rb
@@ -1,8 +1,34 @@
-class GoodDog
+class Song
+
+  def initialize(lyrics)
+    @lyrics = lyrics
+  end
+
+  def sing_me_a_song()
+    @lyrics.each {|line| puts line }
+  end
 end
 
-sparky = GoodDog.new
-fido = GoodDog.new
-spot = GoodDog.new
+happy_bday = Song.new(["Happy birthday to you",
+           "I don't want to get sued",
+           "So I'll stop right there"])
 
-print GoodDog.last
+bulls_on_parade = Song.new(["They rally around tha family",
+            "With pockets full of shells"])
+
+pokemon_theme = Song.new(["I wanna be the very best",
+			"Like no one ever was",
+			"To catch them is my real test"])
+ 
+santa_claus_is_coming_to_town_lyrics = ["He sees you when you're sleeping",
+			"He knows when you're awake.."]
+
+santa_claus_is_coming_to_town = Song.new(santa_claus_is_coming_to_town_lyrics)
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
+
+pokemon_theme.sing_me_a_song()
+
+santa_claus_is_coming_to_town.sing_me_a_song()
